74d183f8de2e9249d977bd5ec9f30f78e6cecffc
gary909 <garywhite61@hotmail.com>
2023-04-25 12:50:40 +0200

    code working

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 function breakChocolate(n,m) {
-    
+    mySum = (n * m) - 1;
+    if(mySum < 0){
+      return 0;
+    } else {
+      return mySum;
+    }
 }
 
 console.log(breakChocolate(5, 5)); // 24
